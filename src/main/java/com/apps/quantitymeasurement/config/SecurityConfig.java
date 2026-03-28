693209f35755dfa69cb0aec2a26719e9715bb686
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-03-28 13:58:45 +0530

    [Nageshwar] : added allowded port number in security config file

--- src/main/java/com/apps/quantitymeasurement/config/SecurityConfig.java
+++ src/main/java/com/apps/quantitymeasurement/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:5500"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setAllowCredentials(true);
